564faf3fdbd742aa8fece2ab6bc1a0c786f104ae
Blest215 <blest215@kaist.ac.kr>
2018-11-22 22:33:54 +0900

    Add Experiment class

--- experiment.py
+++ experiment.py
@@ -1,6 +1,31 @@
+from abc import abstractmethod
+
+from environment import SingleUserSingleServicePartialObservable3DEnvironment
+from models.observation import EuclideanObservation
+
+
 class Experiment:
-    pass
+    @abstractmethod
+    def reset(self):
+        pass
+
+    @abstractmethod
+    def run(self):
+        pass
 
 
 class EffectDrivenVisualServiceSelectionExperiment(Experiment):
-    pass
+    """
+        EffectDrivenVisualServiceSelectionExperiment
+        
+        Experiment done on the SingleUserSingleServicePartialObservable3DEnvironment
+    """
+    def __init__(self, num_device, width, height, depth, max_speed, observation_range):
+        observation = EuclideanObservation(observation_range=observation_range)
+        env = SingleUserSingleServicePartialObservable3DEnvironment(service_type='visual',
+                                                                    num_device=num_device,
+                                                                    width=width,
+                                                                    height=height,
+                                                                    depth=depth,
+                                                                    observation=observation,
+                                                                    max_speed=max_speed)
