30413238acd9fd891313f97d96e56b6f1f770601
Blest215 <blest215@kaist.ac.kr>
2018-11-22 22:20:46 +0900

    Add Observation, EuclideanObservation model
    Add abstract class Observation for further development of observation models
    Add EuclideanObservatio model, that restricts sight of the user according to simple Euclidean distance

---
+++ models/observation.py
@@ -0,0 +1,23 @@
+from abc import abstractmethod
+
+
+class Observation:
+    """ Observation: abstract class for Observation models """
+    @abstractmethod
+    def get_observation(self, user, services):
+        pass
+
+
+class EuclideanObservation(Observation):
+    def __init__(self, observation_range):
+        self.observation_range = observation_range
+
+    def get_observation(self, user, services):
+        """ get_observation: return the Euclidean-distance-based partial observation on the environment """
+        service_observation = [service for service in services
+                               if user.distance(service.device) <= self.observation_range]
+        """ return objects, rather than matrix: agent will transform the observation into matrix """
+        return {
+            "user": user,
+            "services": service_observation
+        }
