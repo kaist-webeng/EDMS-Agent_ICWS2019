49252b3254a0cf68c63db232c2d4f4c5c1d748b9
Blest215 <blest215@kaist.ac.kr>
2018-11-22 20:46:14 +0900

    Modify RectangularDirectedMobility
    Modified coordinate restriction of RectangularDirectedMobility from min-max to 0-max
    Add util function: generate random coordinate

--- mobility.py
+++ mobility.py
@@ -38,18 +38,15 @@ class Mobility:
 
 class RectangularDirectedMobility(Mobility):
     """ RectangularDirectedMobility: mobility that has fixed direction and speed, restricted in a rectangular area"""
-    def __init__(self, min_x, max_x, min_y, max_y, min_z, max_z, direction, speed):
-        self.min_x = min_x
-        self.max_x = max_x
-        self.min_y = min_y
-        self.max_y = max_y
-        self.min_z = min_z
-        self.max_z = max_z
+    def __init__(self, width, height, depth, direction, speed):
+        self.width = width
+        self.height = height
+        self.depth = depth
 
         self.direction = direction
         self.speed = speed
 
     def update(self, coordinate):
-        coordinate.update(clamp(coordinate.x, self.min_x, self.max_x),
-                          clamp(coordinate.y, self.min_y, self.max_y),
-                          clamp(coordinate.z, self.min_z, self.max_z))
+        coordinate.update(clamp(coordinate.x, 0, self.width),
+                          clamp(coordinate.y, 0, self.height),
+                          clamp(coordinate.z, 0, self.depth))
--- utils.py
+++ utils.py
@@ -1,2 +1,10 @@
+import random
+from mobility import *
+
+
 def clamp(value, min_value, max_value):
     return max(min_value, min(value, max_value))
+
+
+def generate_random_coordinate(width, height, depth):
+    return Coordinate(x=random.random() * width, y=random.random() * height, z=random.random() * depth)
