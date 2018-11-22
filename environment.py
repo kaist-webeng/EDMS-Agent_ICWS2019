0077c16d3f4acdf0ca710497e3cadec158a99864
Blest215 <blest215@kaist.ac.kr>
2018-11-22 20:47:04 +0900

    Add abstract class Environment
    abstract class Environment specifies required funciton of Environment classes

---
+++ environment.py
@@ -0,0 +1,33 @@
+from abc import abstractmethod
+
+class Environment:
+    """ Environment: abstract class of IoT environments for required methods """
+
+    @abstractmethod
+    def reset(self):
+        pass
+
+    @abstractmethod
+    def solvable(self):
+        pass
+
+    @abstractmethod
+    def step(self, action):
+        pass
+
+    @abstractmethod
+    def update_state(self):
+        pass
+
+    @abstractmethod
+    def get_state(self):
+        pass
+
+    @abstractmethod
+    def get_observation(self):
+        pass
+
+    @abstractmethod
+    def render(self):
+        pass
+
