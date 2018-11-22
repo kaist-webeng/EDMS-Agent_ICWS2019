39ae1957c8b8d40420e00ed8fb0e91341908a3d8
Blest215 <blest215@kaist.ac.kr>
2018-11-22 21:20:34 +0900

    Add class Direction and Vector
    Now Coordinate and Direction inherits Vector, which is for 3-dimensional vectors

--- mobility.py
+++ mobility.py
@@ -1,19 +1,16 @@
 import random
+import math
 import numpy as np
 from abc import abstractmethod
 from utils import clamp
 
 
-class Coordinate:
-    """ Coordinate: class that represents coordinate of a physical entity """
+class Vector:
+    """ Vector: class of 3-dimensional vector for Coordinate and Direction """
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
-
-    def distance(self, other):
-        assert type(other) == Coordinate
-        return np.sqrt(np.square(self.x - other.x) + np.square(self.y - other.y) + np.square(self.z - other.z))
 
     def get(self):
         return self.x, self.y, self.z
@@ -27,8 +24,32 @@ class Coordinate:
         return "(X:{x}, Y:{y}), Z:{z}".format(x=self.x, y=self.y, z=self.z)
 
 
+class Coordinate(Vector):
+    """ Coordinate: class that represents coordinate of a physical entity in a 3-dimensional space """
+    def distance(self, other):
+        """ distance: calculate Euclidean distance between coordinates"""
+        assert type(other) == Coordinate
+        return np.sqrt(np.square(self.x - other.x) + np.square(self.y - other.y) + np.square(self.z - other.z))
+
+
 def generate_random_coordinate(width, height, depth):
     return Coordinate(x=random.random() * width, y=random.random() * height, z=random.random() * depth)
+
+
+class Direction(Vector):
+    """ Direction: class that represents direction of a physical entity in a 3-dimensional space """
+    def __init__(self, x, y, z):
+        """ Direction should be a unit vector """
+        denominator = np.square(x) + np.square(y) + np.square(z)
+        unit_x = np.sqrt(np.square(x)/denominator)
+        unit_y = np.sqrt(np.square(y)/denominator)
+        unit_z = np.sqrt(np.square(z)/denominator)
+        assert np.square(unit_x) + np.square(unit_y) + np.square(unit_z) == 1
+        Vector.__init__(self, unit_x, unit_y, unit_z)
+
+
+def generate_random_direction():
+    return Direction(random.random(), random.random(), random.random())
 
 
 class Mobility:
@@ -48,6 +69,8 @@ class RectangularDirectedMobility(Mobility):
         self.height = height
         self.depth = depth
 
+        """ direction: direction of the mobility, Direction class """
+        assert type(direction) == Direction
         self.direction = direction
         self.speed = speed
 
