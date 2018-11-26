5b79266430beab9262cc2d7b5b93023540daed0e
Blest215 <blest215@kaist.ac.kr>
2018-11-26 14:05:52 +0900

    Add tensorboard.bat file and variable_summaries function
    For further visualization on tensorboard

--- utils.py
+++ utils.py
@@ -1,3 +1,16 @@
+import tensorflow as tf
+
+
 def clamp(value, min_value, max_value):
     return max(min_value, min(value, max_value))
 
+
+def variable_summaries(var, varname):
+    with tf.name_scope('summaries-'+varname):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean)
+        stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.summary.scalar('stddev', stddev)
+        tf.summary.scalar('max', tf.reduce_max(var))
+        tf.summary.scalar('min', tf.reduce_min(var))
+        # tf.summary.histogram('histogram', var)
