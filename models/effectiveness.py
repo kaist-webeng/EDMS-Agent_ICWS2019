4636bb2767ce52be4d7f07f5d724b02e0998d674
Blest215 <blest215@kaist.ac.kr>
2018-12-03 13:35:12 +0900

    Add head orientation rule of visual effectiveness

--- models/effectiveness.py
+++ models/effectiveness.py
@@ -1,6 +1,8 @@
 import numpy as np
 from abc import abstractmethod
 
+from .mobility import Vector
+
 
 class Effectiveness:
     """ Effectiveness: abstract class for defining service effectiveness models """
@@ -41,10 +43,19 @@ class VisualEffectiveness(Effectiveness):
         """
         user_face = user.infer_orientation()
         device_face = service.device.orientation.face.get_vector_part()
-        cosine = user_face.dot(device_face) / (user_face.size() * device_face.size())
-        
-        if cosine < 0.5:
+        cosine_face_angle = user_face.dot(device_face) / (user_face.size() * device_face.size())
+        if cosine_face_angle < 0.5:
             # angle between user sight and device face is larger than 60 degree
+            return 0
+
+        """
+            head of the visual display should be close to the user's head
+        """
+        user_head = Vector(0, 0, 1)
+        device_head = service.device.orientation.head.get_vector_part()
+        cosine_head_angle = user_head.dot(device_head) / (user_head.size() * device_head.size())
+        if cosine_head_angle < 0.5:
+            # angle between user head and device head is larger than 60 degree
             return 0
 
         return 1
