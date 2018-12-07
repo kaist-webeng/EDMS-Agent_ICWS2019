b2aabd2cbc504da50e6659255ed1ca96221e5d38
Blest215 <blest215@kaist.ac.kr>
2018-12-08 00:41:45 +0900

    Improve agent selection in experiment

--- experiment.py
+++ experiment.py
@@ -20,7 +20,7 @@ class EffectDrivenVisualServiceSelectionExperiment(Experiment):
         
         Experiment done on the SingleUserSingleServicePartialObservable3DEnvironment
     """
-    def __init__(self, num_device, width, height, depth, max_speed, observation_range, num_episode, num_step, memory_size, batch_size, learning_rate, discount_factor):
+    def __init__(self, num_device, width, height, depth, max_speed, observation_range, num_episode, num_step, memory_size, batch_size, learning_rate, discount_factor, agent):
         # observation = EuclideanObservation(observation_range=observation_range)
         observation = FullObservation()
         effectiveness = VisualEffectiveness()
@@ -38,22 +38,25 @@ class EffectDrivenVisualServiceSelectionExperiment(Experiment):
         self.batch_size = batch_size
 
         """ In the code, only one agent should be constructed, Otherwise, error occurs in summary """
-        # random_agent = RandomSelectionAgent("Random", self.env, self.num_episode, self.num_step)
-        # nearest_agent = NearestSelectionAgent("Nearest", self.env, self.num_episode, self.num_step)
-        # no_handover_agent = NoHandoverSelectionAgent("NoHandover", self.env, self.num_episode, self.num_step)
-        # greedy_agent = GreedySelectionAgent("Greedy", self.env, self.num_episode, self.num_step)
-        DRRN_agent = DRRNSelectionAgent("DRRN", self.env, self.num_episode, self.num_step,
-                                        learning_rate=learning_rate,
-                                        discount_factor=discount_factor,
-                                        memory_size=self.memory_size,
-                                        batch_size=self.batch_size)
-        self.agent = DRRN_agent
+        if agent == "random":
+            self.agent = RandomSelectionAgent("Random", self.env, self.num_episode, self.num_step)
+        if agent == "nearest":
+            self.agent = NearestSelectionAgent("Nearest", self.env, self.num_episode, self.num_step)
+        if agent == "nohandover":
+            self.agent = NoHandoverSelectionAgent("NoHandover", self.env, self.num_episode, self.num_step)
+        if agent == "greedy":
+            self.agent = GreedySelectionAgent("Greedy", self.env, self.num_episode, self.num_step)
+        if agent == "DRRN":
+            self.agent = DRRNSelectionAgent("DRRN", self.env, self.num_episode, self.num_step,
+                                            learning_rate=learning_rate,
+                                            discount_factor=discount_factor,
+                                            memory_size=self.memory_size,
+                                            batch_size=self.batch_size)
 
     def reset(self):
         self.env.reset()
 
     def run(self):
-
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
 
