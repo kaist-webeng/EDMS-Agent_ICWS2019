632195a0d74b83be70a6435dc1db8b64b1c5af6c
Blest215 <blest215@kaist.ac.kr>
2018-12-07 16:31:18 +0900

    Implement addition, subtraction of Vectors

--- models/mobility.py
+++ models/mobility.py
@@ -34,6 +34,18 @@ class Vector:
     def __neg__(self):
         return Vector(-self.x, -self.y, -self.z)
 
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __radd__(self, other):
+        return Vector(other.x + self.x, other.y + self.y, other.z + self.z)
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    def __rsub__(self, other):
+        return Vector(other.x - self.x, other.y - self.y, other.z - self.z)
+
     def __mul__(self, other):
         return Vector(self.x * other, self.y * other, self.z * other)
 
